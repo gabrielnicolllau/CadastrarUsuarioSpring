31659ae89cd0a2b910f14505d72ad8dcc7c9d6e5
Gabriel Nicolau de Sousa <gabrielnicolllau1@gmail.com>
2024-11-30 18:30:18 -0300

    Adicionando os demais campos e melhoras o css

--- src/main/java/com/br/CadastroDeUsuarios/model/Usuario.java
+++ src/main/java/com/br/CadastroDeUsuarios/model/Usuario.java
@@ -6,16 +6,17 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 public class Usuario {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "O nome não pode estar vazio")
-    @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
     private String nome;
-
-    @NotBlank(message = "O email não pode estar vazio")
     private String email;
+    private String senha;
+
+    @Column(length = 1000) // Permite textos longos
+    private String permissoes;
 
     // Getters e Setters
     public Long getId() {
@@ -40,6 +41,22 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getPermissoes() {
+        return permissoes;
+    }
+
+    public void setPermissoes(String permissoes) {
+        this.permissoes = permissoes;
     }
 
 }
